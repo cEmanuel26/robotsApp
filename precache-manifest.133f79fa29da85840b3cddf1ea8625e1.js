ae9bdf033d96428842997421c8bdfabce36fe44b
Mr.M <emz@Mr.M>
2020-05-09 15:03:57 +0300

    Updates

---
+++ precache-manifest.133f79fa29da85840b3cddf1ea8625e1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16fb62fc4d612139219fee9f27e54631",
+    "url": "/cEmanuel26/robotsApp/index.html"
+  },
+  {
+    "revision": "c298cdfa16a97262f7bf",
+    "url": "/cEmanuel26/robotsApp/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "1727a90f8fabdea28e9f",
+    "url": "/cEmanuel26/robotsApp/static/css/main.4ffdcc73.chunk.css"
+  },
+  {
+    "revision": "c298cdfa16a97262f7bf",
+    "url": "/cEmanuel26/robotsApp/static/js/2.27c06634.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cEmanuel26/robotsApp/static/js/2.27c06634.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1727a90f8fabdea28e9f",
+    "url": "/cEmanuel26/robotsApp/static/js/main.4375ecb0.chunk.js"
+  },
+  {
+    "revision": "83c8caf7577d7e8f5abc",
+    "url": "/cEmanuel26/robotsApp/static/js/runtime-main.69224bd0.js"
+  },
+  {
+    "revision": "5ce3fc11b995237a76e13dba89a9b3ea",
+    "url": "/cEmanuel26/robotsApp/static/media/Robot World.5ce3fc11.woff"
+  }
+]);
