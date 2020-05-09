c4493a6b21b6dadf72d7080c72a8ea49cdeedb07
Mr.M <emz@Mr.M>
2020-05-09 14:03:55 +0300

    Robots App

---
+++ src/components/Scroll.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Scroll = (props) => {
+  return (
+    <div
+      style={{
+        overflowY: 'scroll',
+        border: '1px solid black',
+        height: '100vh',
+      }}
+    >
+      {props.children}
+    </div>
+  );
+};
+export default Scroll;
